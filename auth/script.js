f2c63bb196fce14b2bce66b80a0e0cb111319961
webwork24 <31735275+webwork24@users.noreply.github.com>
2024-09-12 13:51:51 +0200

    Update script.js

--- auth/script.js
+++ auth/script.js
@@ -1,8 +1,11 @@
+import {IP} from '../constants';
+console.log('ip: ' + IP);
+
 const urlParams = window.location.search;
 if (urlParams.length == 0) {
-    console.log('https://www.youtube.com/');
+    console.log('no parameters');
     window.location.href = 'https://www.youtube.com/';
 } else {
-    console.log('https://www.youtube.com/watch' + urlParams);
+    console.log('there are parameters: ' + urlParams);
     window.location.href = 'https://www.youtube.com/watch' + urlParams;
 }

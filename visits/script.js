4a56efc7e183d42bf9c301d05d9bf4523f9c2523
webwork24 <31735275+webwork24@users.noreply.github.com>
2025-03-09 15:25:18 +0100

    Update script.js

--- visits/script.js
+++ visits/script.js
@@ -2,11 +2,9 @@ import {IP} from '../constants.js';
 console.log('ip: ' + IP);
 
 const urlParams = window.location.search;
-const urlRef = 'http://' + IP + ':8085/queue/home';
+const urlRef = 'https://' + IP + ':8085/queue/home';
 if (urlParams.length == 0) {
-    console.log('no parameters');
     window.location.href = urlRef;
 } else {
-    console.log('there are parameters: ' + urlParams);
     window.location.href = urlRef + urlParams;
 }

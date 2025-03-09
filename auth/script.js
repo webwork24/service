8dfbdae1835b2b557ae54fe08f7a9383cdfbbf9f
webwork24 <31735275+webwork24@users.noreply.github.com>
2025-03-09 15:25:56 +0100

    Update script.js

--- auth/script.js
+++ auth/script.js
@@ -4,9 +4,7 @@ console.log('ip: ' + IP);
 const urlParams = window.location.search;
 const urlRef = 'https://' + IP + ':8082/auth-api/auth';
 if (urlParams.length == 0) {
-    console.log('no parameters');
     window.location.href = urlRef;
 } else {
-    console.log('there are parameters: ' + urlParams);
     window.location.href = urlRef + urlParams;
 }

6af53253a20b31967d79aca2fbb88600f78e33bb
webwork24 <31735275+webwork24@users.noreply.github.com>
2025-09-28 11:58:01 +0200

    Update activity.js

--- activity/activity.js
+++ activity/activity.js
@@ -3,7 +3,7 @@ function shouldSendEvent(data) {
 }
 
 function sendEvent(data) {
-  const urlRef = 'https://worker24.click:8086/activity';
+  const urlRef = 'https://worker24.click:8086/activity/save';
   const activityCode = document.getElementById("activityScript").getAttribute("activity-code");
   
   if (!shouldSendEvent(data) || !activityCode) {
@@ -28,5 +28,8 @@ document.addEventListener('click', function(event) {
   sendEvent({ type: 'click', 
               targetTagName: event.target.tagName,
               targetText: event.target.text,
-              targetAttributes: event.target.attributes });
+              targetClassName: event.target.className,
+              targetAttributes: event.target.attributes,
+              targetBaseURI: event.target.baseURI,
+              targetHost: event.target.host });
 });

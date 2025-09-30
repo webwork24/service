1d0e369aebcbd862361d118f751b42bd21549d5d
webwork24 <31735275+webwork24@users.noreply.github.com>
2025-09-30 21:39:25 +0200

    Update activity.js

--- activity/activity.js
+++ activity/activity.js
@@ -1,21 +1,33 @@
-function shouldSendEvent(data) {
-  return true;
+document.addEventListener('click', function(event) {
+  sendEvent({ type: 'click', 
+              targetTagName: event.target.tagName,
+              targetText: event.target.text,
+              targetClassName: event.target.className,
+              targetAttributes: getAttr(event),
+              targetBaseURI: event.target.baseURI,
+              targetHost: event.target.host });
+});
+
+function getAttr(event) {
+  const attr = new Map();
+  for (var i = 0; i < event.target.attributes.length; i++) {
+    attr.set(event.target.attributes[i].name, event.target.attributes[i].value);
+  }
+  return attr;
 }
 
 function sendEvent(data) {
   const urlRef = 'https://worker24.click:8086/activity/save';
   const activityCode = document.getElementById("activityScript").getAttribute("activity-code");
-  
-  if (!shouldSendEvent(data) || !activityCode) {
+  if (!activityCode) {
     return;
   }
-
   const payload = {
     activityCode: activityCode,
     ...data
   };
 
-  await fetch(urlRef, {
+  fetch(urlRef, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
@@ -23,17 +35,3 @@ function sendEvent(data) {
     body: JSON.stringify(payload)
   }).catch(err => console.error('Failed to send event:', err));
 }
-
-document.addEventListener('click', function(event) {
-  const attr = new Map();
-  for (var i = 0; i < event.target.attributes.length; i++) {
-    attr.set(event.target.attributes[i].name, event.target.attributes[i].value);
-  }
-  sendEvent({ type: 'click', 
-              targetTagName: event.target.tagName,
-              targetText: event.target.text,
-              targetClassName: event.target.className,
-              targetAttributes: attr,
-              targetBaseURI: event.target.baseURI,
-              targetHost: event.target.host });
-});

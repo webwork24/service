afde5903710f0543973362f3e5ad14121707d302
webwork24 <31735275+webwork24@users.noreply.github.com>
2026-02-07 18:00:56 +0100

    Delete auth directory

--- auth/script.js
+++
@@ -1,9 +0,0 @@
-import {IP} from '../constants.js';
-
-const urlParams = window.location.search;
-const urlRef = 'https://' + IP + ':8082/auth-api/auth';
-if (urlParams.length == 0) {
-    window.location.href = urlRef;
-} else {
-    window.location.href = urlRef + urlParams;
-}

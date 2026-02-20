90a19d1d897ec7f63beb68b075fb85f5e6118be5
webwork24 <31735275+webwork24@users.noreply.github.com>
2026-02-20 22:28:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ document.getElementById('goButton').addEventListener('click', function() {
         window.location.href = select.value;
     }
 });
+
+function openModal() {
+    document.getElementById("helpModal").classList.add("active");
+}
+
+function closeModal() {
+    document.getElementById("helpModal").classList.remove("active");
+}
+
+window.addEventListener("click", function(e) {
+    const modal = document.getElementById("helpModal");
+    if (e.target === modal) {
+        closeModal();
+    }
+});

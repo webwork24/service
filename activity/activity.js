69fb343fede38cd4dfa62c970d5ca54c61e80dcb
webwork24 <31735275+webwork24@users.noreply.github.com>
2026-03-16 17:37:53 +0100

    Update activity.js

--- activity/activity.js
+++ activity/activity.js
@@ -52,12 +52,12 @@ function sendEvent(data, activityCode) {
 async function getUserInfo() {
   const userAgent = navigator.userAgent;
 
-  const response = await fetch('https://ipwho.is/');
+  const response = await fetch('http://ip-api.com/json/');
   const data = await response.json();
 
   const userInfo = {
-    ip: data.ip,
-    region: data.region,
+    ip: data.query,
+    region: data.regionName,
     country: data.country,
     countryCode: data.country_code,
     city: data.city,

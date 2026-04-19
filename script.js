a332c2e3bd8f16eb955cd8c1fa42ac4c6ee2be98
webwork24 <31735275+webwork24@users.noreply.github.com>
2026-04-19 18:35:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 document.getElementById('goButton').addEventListener('click', function() {
     const select = document.getElementById('pageSelect');
     if (select.value) {
-        window.location.href = select.value;
+        // window.location.href = select.value;
+        checkService(this.href, this.textContent.trim());
     }
 });
 

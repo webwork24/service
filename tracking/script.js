2241c5546d63d40cce6c28d9cd3877aebc217780
webwork24 <31735275+webwork24@users.noreply.github.com>
2026-05-16 03:37:13 +0200

    Create script.js

---
+++ tracking/script.js
@@ -0,0 +1,9 @@
+import {IP} from '../constants.js';
+
+const urlParams = window.location.search;
+const urlRef = 'https://' + IP + ':8088/tracking/home';
+if (urlParams.length == 0) {
+    window.location.href = urlRef;
+} else {
+    window.location.href = urlRef + urlParams;
+}

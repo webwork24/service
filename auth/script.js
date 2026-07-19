3ed74c4298d6b76af38c0c59e30a0e127923e42d
webwork24 <31735275+webwork24@users.noreply.github.com>
2026-07-19 23:06:28 +0200

    Create script.js

---
+++ auth/script.js
@@ -0,0 +1,9 @@
+import {IP} from '../constants.js';
+
+const urlParams = window.location.search;
+const urlRef = 'https://auth.' + IP;
+if (urlParams.length == 0) {
+    window.location.href = urlRef;
+} else {
+    window.location.href = urlRef + urlParams;
+}
